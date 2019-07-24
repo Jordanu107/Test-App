1de65e542ae9ec5d2564d61ba1784a6d68124751
Jordanu107 <jordanu@student.unimelb.edu.au>
2019-07-24 18:58:04 +1000

    A bunch of random stuff

--- src/add.js
+++ src/add.js
@@ -1,10 +1,16 @@
+this.addEventListener('click', function(event) {
+    alert("You clicked on something!");
+    const closeBtn = document.getElementById('closeBtn');
+    closeBtn.style.backgroundColor = "blue";
+});
+
 const electron = require('electron');
+alert("2");
 const path = require('path');
 const remote = electron.remote;
-
 const closeBtn = document.getElementById('closeBtn');
 
-closeBtn.addEventListener('click', function(event) {
-    let window = remote.getCurrentWindow();
+this.getElementById('closeBtn').addEventListener('click', function(event) {
+    let window = electron.remote.getCurrentWindow();
     window.close();
 });
